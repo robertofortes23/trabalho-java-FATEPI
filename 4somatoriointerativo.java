cf1ef75223c04c0e82db265587b4c633a87dec89
Robert Bernd <robertbernd23@gmail.com>
2020-02-13 12:35:54 -0300

    questao add

--- 4somatoriointerativo.java
+++ 4somatoriointerativo.java
@@ -1,1 +1,21 @@
-    
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Numero inicial ");
+        int inicio = in.nextInt();
+        System.out.println("Numero final ");
+        int fim = in.nextInt();
+
+        int soma = 0;
+
+        while (inicio <= fim) {
+            soma += inicio;
+            inicio += 1;
+        }
+        System.out.println("Somatorio final de todos os numeros é " + soma);
+    }
+}
